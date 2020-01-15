4e1adb55be4e2008995064c9873821ffaae6396c
Mark Zheng Ma <mazheng1022@hotmail.com>
2020-01-15 14:11:19 +0800

    egg-mongoose, validate and router group set up

--- config/config.default.js
+++ config/config.default.js
@@ -23,6 +23,20 @@ module.exports = appInfo => {
     // myAppName: 'egg',
   };
 
+  config.security = {
+    csrf: {
+      ignoreJSON: true,
+    },
+  };
+
+  config.mongoose = {
+    url: process.env.EGG_MONGODB_URL || 'mongodb://127.0.0.1:27017/simpotouch',
+    server: {
+      poolSize: 40,
+    },
+    option: { useUnifiedTopology: true },
+  };
+
   return {
     ...config,
     ...userConfig,
--- config/plugin.js
+++ config/plugin.js
@@ -1,9 +1,16 @@
 'use strict';
 
-/** @type Egg.EggPlugin */
-module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
+exports.routerGroup = {
+  enable: true,
+  package: 'egg-router-group',
+};
+
+exports.mongoose = {
+  enable: true,
+  package: 'egg-mongoose',
+};
+
+exports.validate = {
+  enable: true,
+  package: 'egg-validate',
 };
