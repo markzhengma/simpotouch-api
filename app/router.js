81959bdf3d10d25fe9ff7b39885308842d79d67f
Mark Zheng Ma <mazheng1022@hotmail.com>
2020-01-29 13:42:01 +0800

    remove api from path

--- app/router.js
+++ app/router.js
@@ -7,11 +7,9 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
 
-  router.group({ prefix: '/api' }, router => {
-    router.group({ prefix: '/v1' }, router => {
-      router.get('/event/all', controller.v1.event.findAll);
-      router.post('/event/single', controller.v1.event.create);
-      router.get('/event/single/:id', controller.v1.event.findWithId);
-    })
+  router.group({ prefix: '/v1' }, router => {
+    router.get('/event/all', controller.v1.event.findAll);
+    router.post('/event/single', controller.v1.event.create);
+    router.get('/event/single/:id', controller.v1.event.findWithId);
   })
 };
