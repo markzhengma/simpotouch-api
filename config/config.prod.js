349bcd66d522bec60205f58de4106e8a0f5770e5
Mark Zheng Ma <mazheng1022@hotmail.com>
2020-05-06 22:44:21 +0800

    fix errors

--- config/config.prod.js
+++ config/config.prod.js
@@ -9,9 +9,6 @@ module.exports = () => {
    **/
   const config = exports = {};
 
-  // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1578536605388_1366';
-
   // add your middleware config here
   config.middleware = [];
 
