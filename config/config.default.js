08cb66f354747b6d8818730fe84566d2054da49a
Mark Zheng Ma <mazheng1022@hotmail.com>
2020-05-06 22:41:26 +0800

    fix errors

--- config/config.default.js
+++ config/config.default.js
@@ -31,6 +31,5 @@ module.exports = appInfo => {
 
   return {
     ...config,
-    ...userConfig,
   };
 };
--- config/config.prod.js
+++ config/config.prod.js
@@ -25,6 +25,5 @@ module.exports = () => {
 
   return {
     ...config,
-    ...userConfig,
   };
 };
